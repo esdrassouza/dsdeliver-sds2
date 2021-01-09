8e8a567144602473c4d2dfde32076b42eaa37da4
unknown <esdrascsouza@hotmail.com>
2021-01-09 00:53:22 -0300

    Arquivo api alterado

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { OrderPayload } from './Orders/types';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = ' https://esdras-sds-2.herokuapp.com';
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
-export const fetchProducts = () =>{
-   return axios(`${API_URL}/products`);
+export function fetchProducts(){
+     return axios(`${API_URL}/products`);
 }
 
-export const fetchLocalMapBox =  (local :string)=>{
+export function fetchLocalMapBox (local :string){
    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
 
 
-export const saveOrder = (payload: OrderPayload)=>{
+export function saveOrder(payload: OrderPayload){
    return axios.post(`${API_URL}/orders`, payload);
 }
